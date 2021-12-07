e55ddea2f97523a0f09ab1728e05b032b59c8408
Emanuel Andrecut <emanuel.andrecut@fleio.com>
2021-12-07 13:13:49 +0200

    [fix] #17 Tax rules import

--- whmcsync/sync/tax_rules.py
+++ whmcsync/sync/tax_rules.py
@@ -1,29 +1,35 @@
-import logging
-
 from django.utils.translation import ugettext_lazy as _
 
+from common.logger import get_fleio_logger
 from fleio.billing.models import TaxRule
 from ..models import Tbltax
 
-LOG = logging.getLogger('whmcsync')
+LOG = get_fleio_logger('whmcsync')
 
 
 def sync_tax_rules(fail_fast):
     exception_list = []
     tax_list = []
-    # NOTE: filter all products except for fleio products, for cases when the WHMCS module was used
     for whmcs_tax in Tbltax.objects.all():
         try:
+            if not whmcs_tax.country:
+                LOG.warning(
+                    'Cannot sync WHMCS tax rule {} ({}) that applies to all countries because '
+                    'Fleio does not support this scenario.'.format(whmcs_tax.name, whmcs_tax.id)
+                )
+                continue
             fleio_tax, created = TaxRule.objects.update_or_create(level=whmcs_tax.level,
                                                                   state=whmcs_tax.state or None,
                                                                   country=whmcs_tax.country,
                                                                   defaults={'rate': whmcs_tax.taxrate,
                                                                             'name': whmcs_tax.name})
         except Exception as e:
+            LOG.exception(e)
             exception_list.append(e)
             if fail_fast:
                 break
         else:
+            tax_list.append(fleio_tax)
             if created:
                 LOG.info(_('Created tax {} for {}: {}%').format(fleio_tax.name, fleio_tax.country, fleio_tax.rate))
             else:
